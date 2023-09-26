a007bb4879da40846e547f2fcc94389ea969765c
sandeep-risebeond <sandeep@risebeond.in>
2023-09-26 20:49:58 +0530

    first commit

--- navbar.php
+++ navbar.php
@@ -1164,7 +1164,7 @@
             </div>
         </div>
     </form>
-    <!-- Modal -->
+    <!-- Modal --->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
